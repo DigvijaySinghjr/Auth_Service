e7430bee6e5f2fd8ba7dc33dc5125856b0a4dce0
DigvijaySinghjr <neymar9838jr@gmail.com>
2025-07-01 10:55:15 +0530

    Added rules and user_roles through table

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
+const db = require('./models/index');
+const {User, Role} = require('./models/index');
+
 const app = express();
 
 const prepareAndStartSrver = () => {
@@ -13,8 +16,13 @@ const prepareAndStartSrver = () => {
 
     app.use('/api', apiRoutes);
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server started at ${PORT}`); 
+
+        const u1 = await User.findByPk(4);
+        const r1 = await Role.findByPk(1);
+       
+        u1.addRole(r1);
     });
 }
 
